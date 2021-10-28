3afbfea9cf9916e4aa2a2a90c04337eb73fc4c66
backfromsolaris <porraszach@gmail.com>
2021-10-28 17:17:15 -0500

    post, put methods

--- working.py
+++ working.py
@@ -1,28 +1,63 @@
 from fastapi import FastAPI, Path
 from typing import Optional
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
-inventory = {
-    1: {
-        "name": "Milk",
-        "price": 3.99,
-        "type": "Regular"
-    }
-}
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
+
+class UpdateItem(BaseModel):
+    name: Optional[str] = None
+    price: Optional[float] = None
+    brand: Optional[str] = None
+
+
+inventory = {}
+
 
 @app.get("/get-item/{item_id}")
 # pass a type hint for FastAPI to reference (path parameter)
 # gt = greater than, lt = less than, etc.
-def get_item(item_id: int = Path(None, description="ID of item you want to view", gt=0, lt=2)):
+def get_item(item_id: int = Path(None, description="ID of item you want to view", gt=0, lt=3)):
     return inventory[item_id]
 
-@app.get("/get-by-name")
+
+@app.get("/get-by-name/")
 # if there is no path parameter match within the endpoint, it is treated as a query parameter
 # Optional helps to get better auto-completion when parameters are optional
-def get_item(test: int, name: Optional[str] = None):
+def get_item(name: Optional[str] = None):
     for item_id in inventory:
-        if inventory[item_id]["name"] == name:
+        if inventory[item_id].name == name:
             return inventory[item_id]
     return {"Data": "Not found"}
+
+
+@app.post("/create-item/{item_id}")
+def create_item(item_id: int, item: Item):
+    if item_id in inventory:
+        return {"Error": "Item ID already exists"}
+    
+    inventory[item_id] = item
+    return inventory[item_id]
+
+
+@app.put("/update-item/{item_id}")
+def update_item(item_id: int, item: UpdateItem):
+    if item_id not in inventory:
+        return {"Error": "Item ID does not exist"}
+    
+    if item.name != None:
+        inventory[item_id].name = item.name 
+
+    if item.price != None:
+        inventory[item_id].price = item.price
+
+    if item.brand != None:  
+        inventory[item_id].brane = item.brand
+        
+    return inventory[item_id]
